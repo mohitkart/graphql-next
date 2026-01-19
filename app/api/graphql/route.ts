7c84f8aef36f736ba172fec87318b5e678247ad7
Mohit Kumar <mohitkartweb@gmail.com>
2026-01-19 22:19:07 +0530

    graphql with mongo

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -76,4 +76,12 @@ const server = new ApolloServer({
 
 const handler = startServerAndCreateNextHandler(server);
 
-export { handler as GET, handler as POST };
+export function GET(req: Request) {
+  return handler(req);
+}
+
+export function POST(req: Request) {
+  return handler(req);
+}
+
+// export { handler as GET, handler as POST };
